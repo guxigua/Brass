d15babda9dcac414e1351233f0c50b99dc693933
guxigua <428819551@qq.com>
2022-06-18 10:20:59 +0800

    Create usebrass3.cpp

---
+++ usebrass3.cpp
@@ -0,0 +1,69 @@
+// usebrass3.cpp -- polymorphic example
+// compile with acctacb.cpp
+#include <iostream>
+#include <string>
+#include "acctabc.h"
+const int CLIENTS = 4;
+template<typename T> void Swap(T *a, T *b){
+    T temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main()
+{
+   using std::cin;
+   using std::cout;
+   using std::endl;
+
+   AcctABC * p_clients[CLIENTS];
+   std::string temp;
+   long tempnum;
+   long tempclient=0;
+   double tempbal;
+   char kind;
+
+   for (int i = 0; i < CLIENTS; i++)
+   {
+       cout << "Enter client's name: ";
+       getline(cin,temp);
+       cout << "Enter client's account number: ";
+       cin >> tempnum;
+       Swap(&tempnum, &tempclient);
+       cout << "Enter opening balance: $";
+       cin >> tempbal;
+       cout << "Enter 1 for Brass Account or "
+            << "2 for BrassPlus Account: ";
+       while (cin >> kind && (kind != '1' && kind != '2'))
+           cout <<"Enter either 1 or 2: ";
+       if (kind == '1')
+           p_clients[i] = new Brass(temp, tempnum, tempbal);
+       else
+       {
+           double tmax, trate;
+           cout << "Enter the overdraft limit: $";
+           cin >> tmax;
+           cout << "Enter the interest rate "
+                << "as a decimal fraction: ";
+           cin >> trate;
+           p_clients[i] = new BrassPlus(temp, tempnum, tempbal,
+                                        tmax, trate);
+        }
+        while (cin.get() != '\n')
+            continue;
+   }
+   cout << endl;
+   for (int i = 0; i < CLIENTS; i++)
+   {
+       p_clients[i]->ViewAcct();
+       cout << endl;
+   }
+              
+   for (int i = 0; i < CLIENTS; i++)
+   {
+       delete p_clients[i];  // free memory
+   }
+   cout << "Done.\n";    
+   // cin.get();
+   return 0; 
+}
